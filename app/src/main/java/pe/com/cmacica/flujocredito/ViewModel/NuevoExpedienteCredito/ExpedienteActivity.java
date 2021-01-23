10a88bfcc120fdcd26f04a3665729e1f9a4accca
Y099 Raphael Felix Huamani Cupe <Y099@cmacica.com.pe>
2021-01-23 01:07:24 -0500

    feat: Layout ExpedienteActivity

---
+++ app/src/main/java/pe/com/cmacica/flujocredito/ViewModel/NuevoExpedienteCredito/ExpedienteActivity.java
@@ -0,0 +1,15 @@
+package pe.com.cmacica.flujocredito.ViewModel.NuevoExpedienteCredito;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pe.com.cmacica.flujocredito.R;
+
+public class ExpedienteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expediente);
+    }
+}
