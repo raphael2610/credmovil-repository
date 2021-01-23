85c6054aab0a89094bf4cf9ac3f07d1d524750a2
Y099 Raphael Felix Huamani Cupe <Y099@cmacica.com.pe>
2021-01-23 00:56:21 -0500

    feat: Layout ListadoExpedientesActivity, UpdateExpedienteActivity

--- app/src/main/java/pe/com/cmacica/flujocredito/ViewModel/NuevoExpedienteCredito/ListadoExpedientesActivity.java
+++ app/src/main/java/pe/com/cmacica/flujocredito/ViewModel/NuevoExpedienteCredito/ListadoExpedientesActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import pe.com.cmacica.flujocredito.R;
 
-public class AddExpedienteActivity extends AppCompatActivity {
+public class ListadoExpedientesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_expediente);
+        setContentView(R.layout.activity_listado_expedientes2);
     }
 }
--- app/src/main/java/pe/com/cmacica/flujocredito/ViewModel/NuevoExpedienteCredito/UpdateExpedienteActivity.java
+++ app/src/main/java/pe/com/cmacica/flujocredito/ViewModel/NuevoExpedienteCredito/UpdateExpedienteActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import pe.com.cmacica.flujocredito.R;
 
-public class AddExpedienteActivity extends AppCompatActivity {
+public class UpdateExpedienteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_expediente);
+        setContentView(R.layout.activity_update_expediente);
     }
 }
